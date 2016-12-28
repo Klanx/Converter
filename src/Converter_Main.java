3b1858c1f34ccf89d0288ceb2c11e949f61d2ec2
Joshua Behnke <joshuabehnke@outlook.de>
2016-12-28 08:56:29 +0100

    First commit

---
+++ src/Converter_Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Joshua on 28.12.2016.
+ */
+public class Converter_Main {
+    public static void main (String[] args)
+    {
+        // Ausgabe Hello World!
+        System.out.println("Hello World!");
+    }
+}
